0ee6bcd594a58430cd510abc9b67aa9e70726199
Jummate <yakubjumat@gmail.com>
2023-08-17 13:31:58 +0100

    chore: change the background of header
    change the background from transparent to cyan

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <header className="p-5 text-white fixed z-10 w-full">
+    <header className="p-5 bg-cyan-900 text-white fixed z-10 w-full">
       <nav className="flex justify-between items-center">
         <h1>User</h1>
         <FaBars
