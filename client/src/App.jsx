ba6bba6119d7d218cbcceae4daa4b354dfb1b202
Jummate <yakubjumat@gmail.com>
2023-08-17 12:46:50 +0100

    chore: create Layout component
     create Layout component for nested routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,38 @@ import viteLogo from "/vite.svg";
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/about/About";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 // import "./App.css";
 
 function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
   return (
     <Router>
-      <Header />
       <Routes>
         <Route
           path="/"
-          element={<Home />}
-        />
+          element={<Layout />}
+        >
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
